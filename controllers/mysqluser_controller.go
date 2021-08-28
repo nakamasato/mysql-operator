47c53157f4ea10c18d87b56b07191e76f0983306
Masato Naka <masatonaka1989@gmail.com>
2021-08-28 16:57:51 +0900

    enable to create mysql user with secret

--- controllers/mysqluser_controller.go
+++ controllers/mysqluser_controller.go
@@ -19,7 +19,9 @@ package controllers
 import (
 	"context"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -71,7 +73,8 @@ func (r *MySQLUserReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	// Fetch MySQL
 	var mysql cachev1alpha1.MySQL
-	var mysqlNamespacedName = client.ObjectKey{Namespace: req.Namespace, Name: "mysql-sample"} // hard-coded
+	mysqlName := "mysql-sample" // TODO: extract from mysqlUser.mysqlName
+	var mysqlNamespacedName = client.ObjectKey{Namespace: req.Namespace, Name: mysqlName}
 	if err := r.Get(ctx, mysqlNamespacedName, &mysql); err != nil {
 		log.Error(err, "unable to fetch MySQL")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
@@ -83,18 +86,37 @@ func (r *MySQLUserReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close() // 関数がリターンする直前に呼び出される
+	defer db.Close()
 
+	// Create MySQL user if not exists with password `password`
 	err = db.QueryRow("SELECT * FROM mysql.user WHERE User = ?", 1).Scan(&mysqlUser.Name)
 	switch {
 	case err == sql.ErrNoRows:
 		fmt.Println("レコードが存在しません")
 		log.Info("mysql.user doesn't exist. will be created.", "mysqlUser.Name", mysqlUser.Name, "mysqlUser.Namespace", mysqlUser.Namespace)
-		// stmtInsert, err := db.Prepare("INSERT INTO users(name) VALUES(?)")
-		// if err != nil {
-		// 	panic(err.Error())
-		// }
-		// defer stmtInsert.Close()
+		password := "password" // TODO: generate a random password
+		_, err = db.Exec("CREATE USER IF NOT EXISTS '" + mysqlUser.Name + "'@'%' IDENTIFIED BY '" + password + "';")
+		if err != nil {
+			panic(err.Error())
+		}
+		// create Secret with prefix `mysql-` + `<mysqlName>`
+		secretName := "mysql-" + mysqlName + "-" + mysqlUser.Name
+		data := make(map[string][]byte)
+		data["password"] = []byte(password)
+		secret := &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: req.Namespace,
+			},
+		}
+		if _, err := ctrl.CreateOrUpdate(ctx, r.Client, secret, func() error {
+			secret.Data = data
+			log.Info("Successfully created Secret.")
+			return nil
+		}); err != nil {
+			log.Error(err, "Error creating or updating Secret.")
+			return ctrl.Result{}, err
+		}
 
 	case err != nil:
 		panic(err.Error())
