833777a8007aec12b98739629cc07ae29ecfb07a
Masato Naka <masatonaka1989@gmail.com>
2021-09-26 16:30:48 +0900

    fix

--- controllers/mysqluser_controller.go
+++ controllers/mysqluser_controller.go
@@ -52,6 +52,7 @@ type MySQLUserReconciler struct {
 //+kubebuilder:rbac:groups=mysql.nakamasato.com,resources=mysqlusers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=mysql.nakamasato.com,resources=mysqlusers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=mysql.nakamasato.com,resources=mysqlusers/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
