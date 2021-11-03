94dbb3573c0d5920291035932f05f222ffd064a0
Masato Naka <masatonaka1989@gmail.com>
2021-11-04 06:21:47 +0900

    Add controller test

--- controllers/mysqluser_controller_test.go
+++ controllers/mysqluser_controller_test.go
@@ -7,8 +7,10 @@ import (
 	. "github.com/onsi/ginkgo"
 	"github.com/redhat-cop/operator-utils/pkg/util"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 
@@ -28,7 +30,13 @@ var _ = Describe("MySQLUser controller", func() {
 		Expect(err).ToNot(HaveOccurred())
 
 		err = (&MySQLUserReconciler{
-			ReconcilerBase: util.NewReconcilerBase(k8sManager.GetClient(), k8sManager.GetScheme(), k8sManager.GetConfig(), k8sManager.GetEventRecorderFor("mysqluser_controller"), k8sManager.GetAPIReader()),
+			ReconcilerBase: util.NewReconcilerBase(
+				k8sManager.GetClient(),
+				k8sManager.GetScheme(),
+				k8sManager.GetConfig(),
+				k8sManager.GetEventRecorderFor("mysqluser_controller"),
+				k8sManager.GetAPIReader(),
+			),
 			Log:                nil,
 			Scheme:             k8sManager.GetScheme(),
 			MySQLClientFactory: NewFakeMySQLClient,
@@ -50,20 +58,34 @@ var _ = Describe("MySQLUser controller", func() {
 	})
 
 	const (
-		MySQLUserName      = "test-mysql-user"
-		MySQLUserNamespace = "default"
+		MySQLName     = "test-mysql"
+		MySQLUserName = "test-mysql-user"
+		Namespace     = "default"
 	)
 
-	Context("When updating MySQLUser Status", func() {
-		It("Should increase MySQLUser Status.Active count when new Jobs are created", func() {
+	Context("In normal case", func() {
+		It("Should create Secret", func() {
+			By("By creating a new MySQL")
+			mysql := &mysqlv1alpha1.MySQL{
+				TypeMeta:   metav1.TypeMeta{APIVersion: "mysql.nakamasato.com/v1alphav1", Kind: "MySQL"},
+				ObjectMeta: metav1.ObjectMeta{Name: MySQLName, Namespace: Namespace},
+				Spec:       mysqlv1alpha1.MySQLSpec{Host: "localhost", AdminUser: "root", AdminPassword: "password"},
+			}
+			Expect(k8sClient.Create(ctx, mysql)).Should(Succeed())
+
 			By("By creating a new MySQLUser")
 			mysqlUser := &mysqlv1alpha1.MySQLUser{
 				TypeMeta:   metav1.TypeMeta{APIVersion: "mysql.nakamasato.com/v1alphav1", Kind: "MySQLUser"},
-				ObjectMeta: metav1.ObjectMeta{Name: MySQLUserName, Namespace: MySQLUserNamespace},
-				Spec:       mysqlv1alpha1.MySQLUserSpec{MysqlName: "test-mysql"},
+				ObjectMeta: metav1.ObjectMeta{Name: MySQLUserName, Namespace: Namespace},
+				Spec:       mysqlv1alpha1.MySQLUserSpec{MysqlName: MySQLName},
 				Status:     mysqlv1alpha1.MySQLUserStatus{},
 			}
 			Expect(k8sClient.Create(ctx, mysqlUser)).Should(Succeed())
+
+			secret := &v1.Secret{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, client.ObjectKey{Namespace: Namespace, Name: "mysql-test-mysql-" + MySQLUserName}, secret)
+			}).Should(Succeed())
 		})
 	})
 })
