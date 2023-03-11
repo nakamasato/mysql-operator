3fef45219138b60535d825584918e3e1fe33e33f
Masato Naka <masatonaka1989@gmail.com>
2023-03-12 08:15:35 +0900

    error

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -141,6 +141,7 @@ var _ = Describe("E2e", func() {
 				secret := &corev1.Secret{}
 				Consistently(func() bool {
 					err := k8sClient.Get(ctx, client.ObjectKey{Namespace: mysqlNamespace, Name: secretName}, secret)
+					log.Info("secret", "err", err)
 					return errors.IsNotFound(err)
 				}, timeout, interval).Should(BeTrue())
 
