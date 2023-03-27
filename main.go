4c1ff42fd61dfff794ecfdeb2ed11be9367a5f26
Masato Naka <masatonaka1989@gmail.com>
2023-03-28 06:31:39 +0900

    feat: add new crd MySQLDB

--- main.go
+++ main.go
@@ -108,6 +108,13 @@ func main() {
 		panic(err)
 	}
 
+	if err = (&controllers.MySQLDBReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MySQLDB")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
